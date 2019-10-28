ddd556a0060f0692cabcc00fad5d148e94bc188d
Bibin Vasudevan <Bibin.Vasudevan@maersk.com>
2019-10-28 18:43:21 +0530

    stimulation

--- src/main.go
+++ src/main.go
@@ -4,14 +4,26 @@ package main
 import (
     "fmt"
     "net/http"
-    "time"
+    //"time"
     "github.com/gorilla/websocket"
     "log"
-    "bufio"
+    //"bufio"
     "os"
+    "encoding/json"
+
+   //"math/rand"
 
 
 )
+
+type machineData struct {
+    MemoryStats    string
+    DiskUsage string // An unexported field is not encoded.
+    MemoryUsage    string
+    CPUUtilisation    string
+    MapPlotValue string
+
+}
 
 var upgrader = websocket.Upgrader{
     ReadBufferSize:  1024,
@@ -19,7 +31,7 @@ var upgrader = websocket.Upgrader{
 }
 
 func main() {
-	// Create a simple file server
+
     fs := http.FileServer(http.Dir("../public"))
     http.Handle("/", fs)
     file, err := os.Open("access.log")
@@ -27,21 +39,21 @@ func main() {
             log.Fatal(err)
     }
     defer file.Close()
-
+/*
     http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
         conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
 
 
 
         for {
-            // Read message from browser
+
             msgType, msg, err := conn.ReadMessage()
             fmt.Printf(string (msg))
             if err != nil {
                 return
             }
 
-            // Print the message to the console
+
             fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
             msg = []byte("Here is a string....")
 
@@ -61,6 +73,7 @@ func main() {
 
         }
     })
+*/
 
     http.HandleFunc("/param", func(w http.ResponseWriter, r *http.Request) {
         conn1, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
@@ -68,42 +81,37 @@ func main() {
 
 
         for {
-            // Read message from browser
-            //msgType, msg, err := conn.ReadMessage()
-            //fmt.Printf(string (msg))
-            //if err != nil {
-            //    return
-            //}
 
-            // Print the message to the console
-            //fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
-            //msg = []byte("Here is a string....")
+             machinedata1 := machineData{
+                           MemoryStats:    "50",
+                           MemoryUsage: "90",
+                           DiskUsage: "55",
+                           CPUUtilisation: "55",
+                           MapPlotValue: "55",
+             }
+
+           var jsonData []byte
+            jsonData, err := json.Marshal(machinedata1)
+           if err != nil {
+              log.Println(err)
+           }
+           fmt.Println(string(jsonData))
 
 
-            //scanner := bufio.NewScanner(file)
-            //for scanner.Scan() {
-            //    fmt.Println(scanner.Text())
-            //    time.Sleep(2 * time.Second)
-            //    msg = []byte(scanner.Text())
-            //    conn.WriteMessage(msgType, msg)
-
-            //}
-
-            //if err := scanner.Err(); err != nil {
-            //    log.Fatal(err)
-            //}
-            
            msgType1, msg1, err1 := conn1.ReadMessage()
+           
 
            if err1 != nil {
                 return
             }
- 
-          msg1 = []byte(string(55))
+            for{
+     
+           msg1 = []byte(jsonData)
            conn1.WriteMessage(msgType1, msg1)
          
 
         }
+}
     })
     http.ListenAndServe(":8080", nil)
 }
